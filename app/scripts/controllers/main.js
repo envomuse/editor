4488668edcd5bd1a45fe760fad7f000dcf70ed45
walton.wang <walton.wang@sap.com>
2015-06-02 12:06:16 +0800

    i071628 JIRA: BEB- Description: add log decorator

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,8 +8,8 @@
  * Controller of the musicPlayerApp
  */
 angular.module('musicPlayerApp')
-  .controller('MainCtrl',['$scope', 'authServie', 'playerServie', 'dbservice',
-  	function ($scope, authServie, playerServie, dbservice) {
+  .controller('MainCtrl',['$scope', '$log', 'authServie', 'playerServie', 'dbservice',
+  	function ($scope, $log, authServie, playerServie, dbservice) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
@@ -18,7 +18,7 @@ angular.module('musicPlayerApp')
 
     $.when(dbservice.insert())
     .then(function(newDoc) {
-      console.log('newDoc:', newDoc);
+      $log.debug('newDoc:', newDoc);
       $scope.doc = newDoc;
 
       return dbservice.selectAll()
